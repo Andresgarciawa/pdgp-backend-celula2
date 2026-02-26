51f08fdd0f1b8e89379d2fd63b522a6093827fbe
Andresgarciawa <126025955+Andresgarciawa@users.noreply.github.com>
2026-02-25 23:17:03 -0500

    Estructura

---
+++ src/test/java/com/celula2/auth/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celula2.auth.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
